cfb380388a0978634102e00d734dc4bd907fab1c
SpencerCongdon <spencer.congdon@gmail.com>
2019-03-12 20:34:39 -0300

    Added game win
    It's there now

--- Assets/Scripts/Attic.cs
+++ Assets/Scripts/Attic.cs
@@ -4,6 +4,21 @@ public class Attic : MonoBehaviour
 {
     public MeshRenderer m_renderer;
 
+    private float endWait = 0;
+
+    void Update()
+    {
+        if(m_renderer.enabled)
+        {
+            endWait += Time.deltaTime;
+
+            if(endWait>10)
+            {
+                GameManager.Instance.GameWin();
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player" && !m_renderer.enabled)
